d0db4e15e78a2e361748ea80c3b3521f63c0f8d4
Keyuko <devind3d3@gmail.com>
2025-10-01 11:00:45 +0700

    Commit No.4 - Assignment (10 min list & CardView)

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -57,12 +57,68 @@ class MainActivity : AppCompatActivity() {
                     "https://cdn2.thecatapi.com/images/egv.jpg"
                 ),
                 CatModel(
-                        Gender.Unknown,
-                CatBreed.AmericanCurl,
-                "Curious George",
-                "Award winning investigator",
-                "https://cdn2.thecatapi.com/images/bar.jpg"
-            )
+                    Gender.Unknown,
+                    CatBreed.AmericanCurl,
+                    "Curious George",
+                    "Award winning investigator",
+                    "https://cdn2.thecatapi.com/images/bar.jpg"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.ExoticShorthair,
+                    "Binky",
+                    " pedal binding",
+                    "https://cdn2.thecatapi.com/images/139.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.ExoticShorthair,
+                    "Cleo",
+                    "A calm and affectionate cat, perfect for a quiet home.",
+                    "https://cdn2.thecatapi.com/images/123.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Milo",
+                    "Vocal and social, Milo loves to 'talk' to his human companions.",
+                    "https://cdn2.thecatapi.com/images/19j.gif"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.AmericanCurl,
+                    "Nala",
+                    "A sleepy feline with a kitten-like personality even as an adult.",
+                    "https://cdn2.thecatapi.com/images/5dc.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Oscar",
+                    "Loves adventuring especially climbing walls.",
+                    "https://cdn2.thecatapi.com/images/9u6.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.AmericanCurl,
+                    "Zoe",
+                    "Zoe is intelligent and can often be found solving puzzle toys.",
+                    "https://cdn2.thecatapi.com/images/MTU1Njg0MQ.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.BalineseJavanese,
+                    "Leo",
+                    "An adventurous spirit who sees every cup as a new fort.",
+                    "https://cdn2.thecatapi.com/images/4cs.gif"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Bella",
+                    "Quiet and observant, Bella enjoys watching the world from a window.",
+                    "https://cdn2.thecatapi.com/images/5ia.jpg"
+                )
         )
         )
     }
